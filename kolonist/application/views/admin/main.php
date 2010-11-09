6c4d79cd27b1a165187c5361da43d8c36ee90ec9
Karsen Gauss <hi@kar.gs>
2010-11-09 08:38:27 +0000

    Added necessary modules

---
+++ kolonist/application/views/admin/main.php
@@ -0,0 +1,26 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<html>
+<head>
+<title>
+<?php 
+    echo $title; 
+?>
+</title>
+</head>
+<body>
+<h1>
+<?php 
+    echo $title; 
+?>
+</h1>
+<?php
+    echo $menu;
+    
+    echo $content;
+
+    echo $footer;
+?>
+</div>
+</body>
+</html>
+
