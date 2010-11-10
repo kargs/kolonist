70f9f10ea0d7f785b9af19e9e4e94d91a6ef4077
Karsen Gauss <hi@kar.gs>
2010-11-10 14:09:16 +0000

    Removed unnecessary modules

--- kolonist/modules/formo/classes/formo/driver/checkboxes.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Formo_Driver_Checkboxes extends Formo_Driver_Checkboxes_Core {}
--- kolonist/modules/formo/classes/formo/orm/interface.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-interface Formo_ORM_interface extends Formo_ORM_interface_Core {}
