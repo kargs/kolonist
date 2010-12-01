8763d997ed9bce3893811a671982837cd82dd17c
kamkaro123@gmail.com <kamkaro123@gmail.com@a4884933-c785-d620-165e-21403647bfde>
2010-12-01 09:55:25 +0000

    Map, upgrade buildings

--- trunk/kolonist/application/views/user/login.php
+++ trunk/kolonist/application/views/user/login.php
@@ -2,7 +2,7 @@
 <?php form::$values = isset ($values) ? $values : null ?>
 
 <?php echo form::open('user/login') ?>
-<?php echo form::field('username', 'Username', 'input') ?>
-<?php echo form::field('password', 'Password', 'password') ?>
-<?php echo form::submit('submit','Login') ?>
+<?php echo form::field('username', 'Login', 'input') ?>
+<?php echo form::field('password', 'Hasło', 'password') ?>
+<?php echo form::submit('submit','Loguj') ?>
 <?php echo form::close() ?>
