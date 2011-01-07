e19b4d36fb0de375ce527a3a9416bf5910a757e6
karol@gusak.eu <karol@gusak.eu@a4884933-c785-d620-165e-21403647bfde>
2011-01-07 15:34:34 +0000

    Reworked fight, much better

--- trunk/kolonist/application/config/options.php
+++ trunk/kolonist/application/config/options.php
@@ -2,8 +2,8 @@
 
 return array (
 	'cronInterval' => 60, // 60, Seconds to run cron
-	'fightAbsoluteLuckLevel' => 100, // 100, Maximum attack / defense ratio for giving chance to win by luck
-	'fightRatioCap' => 1000, // 1000, Maximum attack / defense ratio after which one can't loose more troops
+	'fightAbsoluteLuckLevel' => 10, // 10, Maximum attack / defense ratio for giving chance to win by luck
+	'fightRatioCap' => 100, // 100, Maximum attack / defense ratio after which one can't loose more troops
 	'fightMaxPercentLoss' => 30, // 30, Maximum percentage of troops one can loose when the ratio is maximal (50% is added to this value)
-	'fightRandomLoss' => 20 // 20, Maximum percentage which can be added (randomly) or substracted from the general loss percentage (can't be more than 100% when added to the above)
+	'fightRandomLoss' => 10 // 10, Maximum percentage which can be added (randomly) or substracted from the general loss percentage
 );
