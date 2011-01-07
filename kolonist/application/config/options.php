f6e2e22e6e705a32967a2c2868590b0e9fbbcf24
Karsen Gauss <hi@kar.gs>
2011-01-07 12:42:39 +0000

    Reworked fight loss ratios

--- kolonist/application/config/options.php
+++ kolonist/application/config/options.php
@@ -1,5 +1,9 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array (
-	'cronInterval' => 60,
+	'cronInterval' => 60, // 60, Seconds to run cron
+	'fightAbsoluteLuckLevel' => 100, // 100, Maximum attack / defense ratio for giving chance to win by luck
+	'fightRatioCap' => 1000, // 1000, Maximum attack / defense ratio after which one can't loose more troops
+	'fightMaxPercentLoss' => 30, // 30, Maximum percentage of troops one can loose when the ratio is maximal (50% is added to this value)
+	'fightRandomLoss' => 20 // 20, Maximum percentage which can be added (randomly) or substracted from the general loss percentage (can't be more than 100% when added to the above)
 );
