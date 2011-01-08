dc6cef5caad7202ac6786c5b1b404ad0a79443c9
Karsen Gauss <hi@kar.gs>
2011-01-08 15:58:37 +0000

    Rules described

---
+++ kolonist/application/views/info/battles.php
@@ -0,0 +1,9 @@
+<div class="textDiv"><a href="<?php echo url::site('info/rules') ?>">Rules</a> &raquo; Battles</div>
+
+<div class="textBlock">
+<p>In order to attack a province, click on it - the dialog window will show up. Click on the "attack" tab. You will see your provinces with armies they have. Use the scroller to choose the amount of armies which will take part in the fight. When you are ready click "battle". The fight is instantaneous - you will see the summary window with various fight information.</p>
+<p>Two types of resources are important for fights. The soldiers, of course - the more the better. But you also have to take care of the armament value for every province. It is very important to have at least as many armament value as there are soldiers in a province. The bigger armament / soldiers ratio, the bigger attack gain of your army. But the opposite is also true - the less armament per one soldier, the worse attack you have. If this ratio is near zero, your army will be worthless no matter how big it is! They can't fight without swords and shields!</p>
+<ul>
+	<li><a href="<?php echo url::site('info/battles2') ?>">Read more</a></li>
+</ul>
+</div>
