6eb5b4e5627036ed7f1e0abbe55cd8281c9383dc
kamkaro123@gmail.com <kamkaro123@gmail.com@a4884933-c785-d620-165e-21403647bfde>
2011-01-09 13:45:48 +0000

    Army move, pname

--- trunk/kolonist/public/js/game/province.js
+++ trunk/kolonist/public/js/game/province.js
@@ -82,21 +82,103 @@ $(function() {
             alert(r.lostDecimal);
             var enemyLost = Math.round(parseFloat(r.lostDecimal)*100);
             var youLost = 100 - enemyLost;
-//            $('#battleResultDialog .enemyLost').html(enemyLost);
-//            $('#battleResultDialog .yourLost').html(youLost);
+            //            $('#battleResultDialog .enemyLost').html(enemyLost);
+            //            $('#battleResultDialog .yourLost').html(youLost);
             $('#battleResultDialog .enemyLost').html(youLost);
             $('#battleResultDialog .yourLost').html(enemyLost);
             $('#enemyDialog').dialog('close');
             $('#battleResultDialog').dialog('open');
         });
     });
+
+    $('#moveSoildiersDlg').dialog({
+        autoOpen: false,
+        modal: true,
+        resizable: false,
+        title: translate('armyMove')
+    });
+    $('.armySlider').slider({
+        min: 0,
+        slide: function(event, ui) {
+            $('#moveSoildiersDlg .armySliderVal').val(ui.value);
+        }
+    });
+    $('.showSoildierMoveDlg').live('click', function(event){
+        event.preventDefault();
+        var pid = $(this).attr('pid');
+        var soldiers = $(this).attr('soldiers');
+        showSoildiersMoveDialog(pid, soldiers);
+    });
+    $('#moveSoildiersDlg .moveArmyAction').live('click', function(event) {
+        event.preventDefault();
+        var pid = $('#moveSoildiersDlg .maPid').val();
+        var sld = $('#moveSoildiersDlg .armySliderVal').val();
+        var npid = $('#moveSoildiersDlg .provinceChoose').val();
+        $.get('json/movesoldiers/'+pid+'/'+npid+'/'+sld, function(data) {
+            var r = null;
+            if((r = parseJSON(data)) === undefined) {
+                return;
+            }
+            updateProvince(pid);
+            processInfo(r.status.code, r.status.message);
+            $('#moveSoildiersDlg').dialog('close');
+        });
+    });
+
+    $('.showProvNameChangeDlg').live('click', function(event) {
+        event.preventDefault();
+        var pid = $(this).attr('pid');
+        var name = provincesAsoc[pid].name;
+        $('#nameChangerDlg .provName').val(name);
+        $('#nameChangerDlg').dialog('open');
+    });
+
+    $('#nameChangerDlg').dialog({
+        autoOpen: false,
+        modal: true,
+        resizable: false,
+        title: translate('changeProvinceNameTitle')
+    });
+    $('#nameChangerDlg .changeNameAction').live('click', function(event) {
+        event.preventDefault();
+        var pid = $(this).attr('pid');
+        var name = $('#nameChangerDlg .provName').val();
+        $.get('json/changeprovincename/'+pid+'/'+name, function(data) {
+            var r = null;
+            if((r = parseJSON(data)) === undefined) {
+                return;
+            }
+            updateProvince(pid);
+            processGameState();
+            processInfo(r.status.code, r.status.message);
+            $('#nameChangerDlg').dialog('close');
+        });
+    });
+    
+
 });
+
+function showSoildiersMoveDialog(pid, armyCnt) {
+    $('#moveSoildiersDlg .armySlider').slider('option', 'value', 0);
+    $('#moveSoildiersDlg .armySlider').slider('option', 'max', armyCnt);
+    $('#moveSoildiersDlg .armyCnt').html(armyCnt);
+    var html = '';
+    $.each(provinces, function(i, p) {
+        if(p.owner.id != userId) return;
+        html += '<option value="'+p.id+'">'+p.name+'</option>';
+    });
+    $('#moveSoildiersDlg .provinceChoose').html(html);
+    $('#moveSoildiersDlg .maPid').val(pid);
+    $('#moveSoildiersDlg').dialog('open');
+}
 
 function showProvince(id) {
     if(provincesAsoc[id].owner.id == userId) {
         $('#provinceView .loaded').css('display', 'none');
         $('#provinceView .loading').css('display', 'block');
-
+        $('.showSoildierMoveDlg').attr('pid', id);
+        $('.insPID').attr('pid', id);
+        
         updateProvince(id);
 
         $('#provinceView').dialog('open');
@@ -160,6 +242,7 @@ function updateProvince(id) {
             for (var resName in resources) {
                 $('div#provinceView .resourcesbar .'+resName).html(resources[resName]);
             }
+            $('.showSoildierMoveDlg').attr('soldiers', resources.soldiers);
             var slots = new Array();
             for(var i=0; i< slotMax; i++) {
                 slots[i] = null;
@@ -175,7 +258,7 @@ function updateProvince(id) {
                         params: [p.id, b.slot_index, b, p.resources],
                         click: function (e, params) {
                             showBuildingUpgrader(params[0], params[2], params[3]);
-//                            upgradeBuilding(params[0], params[1]);
+                        //                            upgradeBuilding(params[0], params[1]);
                         }
                     }
                     balls[balls.length] = {
@@ -209,18 +292,21 @@ function updateProvince(id) {
                     click = function(event, param) {
                         showBuildingChooser(id, param);
                     }
-                    params = {resources: p.resources, slotIndex: i};
-//                    for (var bname in buildings) {
-//                        // sprawdzać ilość resourców
-//                        balls[balls.length] = {
-//                            css: 'bgtoggle develop',
-//                            title: 'Build '+bname,
-//                            params: [p.id, i, bname],
-//                            click: function (e, params) {
-//                                createBuilding(params[0], params[1], params[2]);
-//                            }
-//                        }
-//                    }
+                    params = {
+                        resources: p.resources,
+                        slotIndex: i
+                    };
+                //                    for (var bname in buildings) {
+                //                        // sprawdzać ilość resourców
+                //                        balls[balls.length] = {
+                //                            css: 'bgtoggle develop',
+                //                            title: 'Build '+bname,
+                //                            params: [p.id, i, bname],
+                //                            click: function (e, params) {
+                //                                createBuilding(params[0], params[1], params[2]);
+                //                            }
+                //                        }
+                //                    }
                 } else {
                     balls = slots[i].balls;
                     img = slots[i].img;
