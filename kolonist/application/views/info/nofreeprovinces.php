c64d19a8c50afe0268f9dff847c8c9c0a8b2767f
Karsen Gauss <hi@kar.gs>
2011-01-09 18:13:07 +0000

    Minor changes

---
+++ kolonist/application/views/info/nofreeprovinces.php
@@ -0,0 +1,6 @@
+<div class="textDiv">There are no free provinces available!</div>
+
+<div class="textBlock">
+<p>We are very sorry but all provinces in the game are already taken by other players, which means we cannot assign you to a free province. You cannot join to this session but please be patient - we will start over soon!</p>
+<p><img src="graph/error.png" style="margin:auto;text-align:center" /></p>
+</div>
