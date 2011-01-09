cbee87c9e67025e4adef1fac6e0c13f0dc5f1c8a
Karsen Gauss <hi@kar.gs>
2011-01-09 18:56:54 +0000

    More views

--- kolonist/application/views/failure/guestonly.php
+++ kolonist/application/views/failure/guestonly.php
@@ -1,1 +1,5 @@
-Guest only!
+<div class="textDiv">Guest only!</div>
+
+<div class="textBlock">
+<p>Sorry, you have to be logged out to access this page.</p>
+</div>
--- kolonist/application/views/failure/noaccess.php
+++ kolonist/application/views/failure/noaccess.php
@@ -1,1 +1,5 @@
-No access!
+<div class="textDiv">No access!</div>
+
+<div class="textBlock">
+<p>Sorry, you have no sufficient priviledges to access this page. If you are logged out, please log in.</p>
+</div>
