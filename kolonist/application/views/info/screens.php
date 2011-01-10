15b20f78b8e86cdf8d2dde50e21710b93dbcbbcd
Karsen Gauss <hi@kar.gs>
2011-01-10 21:00:42 +0000

    Screens, no thumbs

--- kolonist/application/views/info/screens.php
+++ kolonist/application/views/info/screens.php
@@ -1,1 +1,10 @@
 <div class="textDiv">Screens</div>
+
+<a href="/screens/main-screen.png" rel="lightbox[screens]">Main Screen</a>
+<a href="/screens/provinces.png" rel="lightbox[screens]">Provinces</a>
+<a href="/screens/create-new-building.png" rel="lightbox[screens]">Create new building</a>
+<a href="/screens/upgrade-building.png" rel="lightbox[screens]">Upgrade building</a>
+<a href="/screens/building-information.png" rel="lightbox[screens]">Building information</a>
+<a href="/screens/battle-screen.png" rel="lightbox[screens]">Battle screen</a>
+<a href="/screens/won-fight.png" rel="lightbox[screens]">Won fight</a>
+<a href="/screens/lost-fight.png" rel="lightbox[screens]">Lost fight</a>
